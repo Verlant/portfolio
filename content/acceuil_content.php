2ffbecfdfbb927c5defaab4ccc8737b6522ba539
Antoine Verlyck <antoine.verlyck@gmail.com>
2023-01-31 14:51:59 +0100

    jeu video et pas jeux video au singulier

--- content/acceuil_content.php
+++ content/acceuil_content.php
@@ -2,7 +2,7 @@
     <div class="presentation">
         <p>Bonjour, je m'appelle <span class="key-word">Antoine VERLYCK</span>, futur <span class="key-word">Développeur Web Junior</span>, actuellement en formation.</p>
 
-        <p>La passion de la <span class="key-word">programmation</span> me viens d'une autre passion, celle du <span class="key-word">jeux vidéo</span>.</p>
+        <p>La passion de la <span class="key-word">programmation</span> me viens d'une autre passion, celle du <span class="key-word">jeu vidéo</span>.</p>
 
         <p>J'ai voulu <span class="key-word">apprendre</span> de quelle façon ceux-ci étaient construit, ce qui m'a amené a apprendre la programmation.</p>
 
